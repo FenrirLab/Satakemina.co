412245c63ab96b05e7fa63d965abb51ad41b6793
FenrirLab <43561935+FenrirLab@users.noreply.github.com>
2021-05-31 13:12:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+satakemina.co
---
+++ js/swiper.js
@@ -0,0 +1,37 @@
+const slider1 = new Swiper('#slider1', {
+      /*autoplay loop*/
+      spaceBetween: 0,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+const profile = new Swiper('#profile', {
+      /*autoplay loop*/
+      spaceBetween: 0,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
